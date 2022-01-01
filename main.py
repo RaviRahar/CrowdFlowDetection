704074ccdd2f8b259a52c36bd98f2d67ea61eb37
RaviRahar <ravirahar33@gmail.com>
2022-01-01 20:32:59 +0530

    added streaklines.png

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import numpy as np
 import cv2 as cv
-from opticalflow import *
-from watershed import *
-from streaklines import *
-from similarities import *
+from CrowdFlowSegmentation import *
 
-cap = cv.VideoCapture('UCF_CrowdsDataset/001-0438.mov')
+stride=20
+
+cap = cv.VideoCapture('UCF_CrowdsDataset/3687-18_70.mov')
 
 no_of_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 ret, frame = cap.read()
 if not ret:
     print('No frames grabbed!\n')
 
-crowdFlow = CrowdFlowSegmentation(frame, no_of_frames)
+crowdFlow = CrowdFlowSegmentation(frame, no_of_frames, stride)
 
 while True:
     ret, frame = cap.read()
     if not ret:
         print('No frames grabbed!\n')
         break
-    modified_frame = crowdFlow.to_dense(frame)
+#    modified_frame = crowdFlow.to_avg_opt_flow(frame)
+    modified_frame = crowdFlow.to_quiverplot(frame)
 #    modified_frame = crowdFlow.to_streaklines(modified_frame)
 #    modified_frame = crowdFlow.to_similarities(modified_frame)
 #    modified_frame = crowdFlow.to_watershed(modified_frame)
-    cv.imshow('window', modified_frame)
+#    cv.imshow('window', modified_frame)
 
     k = cv.waitKey(30) & 0xff
     if (k == 27 or k == ord('q')):
